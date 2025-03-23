f423a9a546da3e2f6c5c97012fd1d900ba187baf
front-mokh <142145455+front-mokh@users.noreply.github.com>
2025-03-23 15:02:01 +0100

    test

---
+++ src/app/[locale]/(site)/submissions/new/page.tsx
@@ -0,0 +1,10 @@
+import SubmissionForm from "@/components/submission/SubmissionForm";
+import React from "react";
+
+export default function NewSubmissionPage() {
+  return (
+    <div className="container py-8">
+      <SubmissionForm />
+    </div>
+  );
+}
