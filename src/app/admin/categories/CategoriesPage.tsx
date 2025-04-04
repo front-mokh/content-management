5e2b10fe5d3c216212b5c12bb3aa32285eb9931c
front-mokh <142145455+front-mokh@users.noreply.github.com>
2025-04-04 21:01:50 +0100

    Update CategoriesPage.tsx

--- src/app/admin/categories/CategoriesPage.tsx
+++ src/app/admin/categories/CategoriesPage.tsx
@@ -1,23 +1,47 @@
 "use client";
 import AddCategoryDialog from "@/components/custom/AddCategoryDialog";
+import CustomPagination from "@/components/custom/CustomPagination";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { usePagination } from "@/hooks/use-pagination";
 import { Category } from "@prisma/client";
 import React from "react";
 import CategoriesTable from "./CategoriesTable";
-import Page from "@/components/custom/Page";
 
 export default function CategoriesPage({
   categories,
 }: {
   categories: Category[];
 }) {
+  const { currentPage, totalPages, pageItems, handlePageChange } =
+    usePagination<Category>(categories, 8);
   return (
-    <Page
-      title="Gestion des Catégories"
-      description="Créer, modifier et gérer les catégorie"
-      backButtonHref="" 
-      actions={<AddCategoryDialog />}
-    >
-      <CategoriesTable categories={categories} />
-    </Page>
+    <div className="h-full">
+      <Card className="h-full">
+        <CardHeader className="flex flex-row items-center justify-between">
+          <CardTitle>Gestion des Catégories</CardTitle>
+          <AddCategoryDialog />
+        </CardHeader>
+        <CardContent>
+          <CategoriesTable categories={pageItems} />
+        </CardContent>
+        <CardFooter>
+          {totalPages > 1 && (
+            <div className="mt-6">
+              <CustomPagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange}
+              />
+            </div>
+          )}
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
